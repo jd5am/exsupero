078bff713d5ea24be40d6960ffa891c9d2992bfb
Jay <jayd@sapphiregroup.com>
2017-01-23 12:00:52 +0100

    bootstrap theme added

---
+++ exsupero-laravel-website/app/Http/Controllers/WhatWeDoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WhatWeDoController extends Controller
+{
+    public function whatwedo()
+    {
+    	return view ('whatwedo');
+    }
+}
---
+++ exsupero-laravel-website/resources/views/whatwedo.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('title')
+	What We Do
+@endsection
+
+@section('section-1')
+	Lorem ipsum text
+@endsection
+
+@section('section-2')
+	Lorem ipsum text
+@endsection
